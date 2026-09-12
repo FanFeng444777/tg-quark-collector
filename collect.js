5f8570d396e61c5f32d698406768462b56dc7e28
FanFeng444777 <375864673@qq.com>
2026-09-13 01:01:07 +0800

    Update collect.js

--- collect.js
+++ collect.js
@@ -60,7 +60,7 @@ const client = new TelegramClient(new StringSession(sessionStr), apiId, apiHash,
 await client.connect();
 
 const newItems = [];
-const isFirstRun = existingMap.size === 0;
+const isFirstRun = true; // 临时调试
 
 for (const ch of channels) {
   try {
